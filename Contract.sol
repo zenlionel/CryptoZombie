af329df40ebb1e736a0c7961813fa41d1db2401f
Leo Zen-ruffinen <leozen@Leos-MBP.home>
2018-05-27 13:37:42 -0700

    require for one zombie per user

--- Contract.sol
+++ Contract.sol
@@ -30,7 +30,7 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string _name) public {
-        // start here
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
