5bff9613b848ec573d1145aacdbfb739a59b0009
Leo Zen-ruffinen <leozen@Leos-MacBook-Pro.local>
2018-05-28 15:39:26 -0700

    fixed duplicate file

--- zombieFeeding.sol
+++ zombieFeeding.sol
@@ -1,38 +1,17 @@
 pragma solidity ^0.4.19;
 
-contract ZombieFactory {
+import "./zombiefactory.sol";
 
-    event NewZombie(uint zombieId, string name, uint dna);
+// Create KittyInterface here
 
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+contract ZombieFeeding is ZombieFactory {
 
-    struct Zombie {
-        string name;
-        uint dna;
-    }
-
-    Zombie[] public zombies;
-
-    mapping (uint => address) public zombieToOwner;
-    mapping (address => uint) ownerZombieCount;
-
-    function _createZombie(string _name, uint _dna) private {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
-        NewZombie(id, _name, _dna);
-    }
-
-    function _generateRandomDna(string _str) private view returns (uint) {
-        uint rand = uint(keccak256(_str));
-        return rand % dnaModulus;
-    }
-
-    function createRandomZombie(string _name) public {
-        require(ownerZombieCount[msg.sender] == 0);
-        uint randDna = _generateRandomDna(_name);
-        _createZombie(_name, randDna);
-    }
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    _createZombie("NoName", newDna);
+  }
 
 }
