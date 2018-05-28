d9700353df5559c9710f234683159a6bc8ae22e0
Leo Zen-ruffinen <leozen@Leos-MacBook-Pro.local>
2018-05-28 15:48:51 -0700

    Feeding cryptoKitty Genes to zombies

--- zombieFeeding.sol
+++ zombieFeeding.sol
@@ -2,9 +2,24 @@ pragma solidity ^0.4.19;
 
 import "./zombiefactory.sol";
 
-// Create KittyInterface here
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
 
 contract ZombieFeeding is ZombieFactory {
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
